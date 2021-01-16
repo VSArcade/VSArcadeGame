b2eabe9929cac85df949962312d279ac46c24cf3
Daniel Liu <mr.picklepinosaur@gmail.com>
2021-01-16 10:12:50 -0500

    basic physics

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,36 @@
-import { greet } from './game'
+import { Engine, Render, World, Bodies, Body} from 'matter-js'
 
-greet();
+// create an engine
+var engine: Engine = Engine.create();
+
+// create a renderer
+var render: Render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        wireframes: false
+    }
+});
+render.canvas.id = 'game-canvas';
+
+// create two boxes and a ground
+var boxA: Body = Bodies.rectangle(400, 200, 80, 80, {
+        render: {
+            fillStyle: 'red',
+            lineWidth: 3
+        } 
+    }
+);
+var boxB: Body = Bodies.rectangle(450, 50, 80, 80);
+var ground: Body = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+// add all of the bodies to the world
+World.add(engine.world, [boxA, boxB, ground]);
+
+// run the engine
+Engine.run(engine);
+
+// run the renderer
+Render.run(render);
