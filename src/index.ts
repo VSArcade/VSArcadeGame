ce93b8c752977a58f3ca8f563266a82cee44a666
Eric Huang <eric.haoran.huang@gmail.com>
2021-01-16 15:53:55 -0500

    doing stuff for vscode extension

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,64 @@
 import Matter from 'matter-js';
 import { Engine, Render, World, Bodies, Body } from 'matter-js'
-import {GameWord} from './GameWord'
+import { GameWord } from './GameWord'
 import { initListeners } from './reciever'
 import VSAEngine from './engine'
 import Player from './game/player'
+import { EventType, EventInfo, EventData, InitEventInfo } from './types/EventData.types'
 
 
-initListeners();
+// initListeners();
 
+window.addEventListener('message', (event: MessageEvent) => {
 
-var text = "bhihih poo and pee"
+  const data: EventData = event.data;
 
-// create an engine
-var engine: Engine = Engine.create();
+  switch (data.eventType) {
 
-function createGameWords(text:string):GameWord[]{
-    var curLength= 400;
-    return text.split(' ').map((word,index) => {
-        console.log(curLength)
-        var gm = new GameWord(word,curLength,200,10*word.length, 20)
-        curLength+=10*word.length 
-        return gm
-        
+    case EventType.Init:
 
+      const info: InitEventInfo = data.eventInfo;
+      console.log(`vscode-background: ${info.styles.background}`);
 
-        
+      break;
+
+    default:
+      console.warn('Illegal event data type');
+
+  }
+
+  let text = event.data.msg.code;
+
+  // create an engine
+  var engine: Engine = Engine.create();
+
+  function createGameWords(text: string): GameWord[] {
+    var curLength = 400;
+    return text.split(' ').map((word, index) => {
+      console.log(curLength)
+      var gm = new GameWord(word, curLength, 200, 10 * word.length, 20)
+      curLength += 10 * word.length
+      return gm
     });
+  }
+
+  var words = createGameWords(text)
+  var ground: Body = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+  // add all of the bodies to the world
+  World.add(engine.world, ground);
+  words.forEach((word) => {
+    World.add(engine.world, word.body)
+  })
 
 
-}
-var words = createGameWords(text)
-var ground: Body = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
-// add all of the bodies to the world
-World.add(engine.world, ground);
-words.forEach((word)=>{
-    World.add(engine.world,word.body)
-})
+  // run the engine
+  Engine.run(engine);
 
+  function render() {
+    var canvas = <HTMLCanvasElement>document.getElementById('game-canvas')
 
-
-// run the engine
-Engine.run(engine);
-
-function render () {
-    var canvas = <HTMLCanvasElement> document.getElementById('game-canvas')
-    
     var context = canvas.getContext('2d')
 
     window.requestAnimationFrame(render)
@@ -64,7 +77,7 @@ function render () {
         var color = '#FF0000'
 
         var content = word.text//this is the string
-        
+
         context!.fillStyle = 'black'
         context!.save()
         context!.translate(word.body.position.x, word.body.position.y)
@@ -80,17 +93,19 @@ function render () {
         context!.restore()
 
       }
-    //   var vertices = word.body.vertices
-    //   context!.moveTo(vertices[0].x, vertices[0].y)
-    //   for (var j = 1; j < vertices.length; j += 1) {
-    //     context!.lineTo(vertices[j].x, vertices[j].y)
-    //   }
-    //   context!.lineTo(vertices[0].x, vertices[0].y)
+      //   var vertices = word.body.vertices
+      //   context!.moveTo(vertices[0].x, vertices[0].y)
+      //   for (var j = 1; j < vertices.length; j += 1) {
+      //     context!.lineTo(vertices[j].x, vertices[j].y)
+      //   }
+      //   context!.lineTo(vertices[0].x, vertices[0].y)
     }
     context!.lineWidth = 1.5
     context!.strokeStyle = '#000000'
     context!.stroke()
-}
-render()
+  }
+
+  render()
 
 
+});
